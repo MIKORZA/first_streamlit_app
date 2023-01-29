3776fcea8e9f1b2ba276341cf680c341f1df58b3
MIKORZA <121402193+MIKORZA@users.noreply.github.com>
2023-01-29 23:45:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output in the screen as table
 streamlit.dataframe(fruityvice_normalized)
-
-update streamlit_app-py
